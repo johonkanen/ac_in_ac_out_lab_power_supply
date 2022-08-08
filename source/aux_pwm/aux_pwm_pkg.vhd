e7d722d2e06ed6d4040517d2b0f748c2bec1f8e2
jari <jari.honkanen@danfoss.com>
2022-08-08 19:37:41 +0300

    add aux pwm package

---
+++ source/aux_pwm/aux_pwm_pkg.vhd
@@ -0,0 +1,71 @@
+library ieee;
+    use ieee.std_logic_1164.all;
+    use ieee.numeric_std.all;
+
+package aux_pwm_pkg is
+
+    type aux_pwm_record is record
+        pwm_is_requested : boolean;
+        pwm_counter : integer range 0 to 2**16-1;
+        duty_ratio : integer range 0 to 2**16-1;
+        period : integer range 0 to 2**16-1;
+        pwm_out : std_logic;
+    end record;
+
+    constant init_aux_pwm : aux_pwm_record := (false, 0, 0, 500, '0');
+
+    procedure create_aux_pwm (
+        signal aux_pwm_object : inout aux_pwm_record);
+
+    procedure start_aux_pwm (
+        signal aux_pwm_object : out aux_pwm_record);
+
+    procedure stop_aux_pwm (
+        signal aux_pwm_object : out aux_pwm_record);
+
+end package aux_pwm_pkg;
+
+package body aux_pwm_pkg is
+
+    procedure create_aux_pwm
+    (
+        signal aux_pwm_object : inout aux_pwm_record
+    ) is
+        alias m is aux_pwm_object;
+    begin
+        if m.pwm_counter > 0 then
+            m.pwm_counter <= m.pwm_counter -1;
+        else
+            m.pwm_counter <= m.period;
+        end if;
+
+        if m.pwm_counter < m.duty_ratio then
+            m.pwm_out <= '1';
+        else
+            m.pwm_out <= '0';
+        end if;
+
+        if not m.pwm_is_requested then
+            m.pwm_counter <= 0;
+            m.pwm_out <= '0';
+        end if;
+
+    end create_aux_pwm;
+
+    procedure start_aux_pwm
+    (
+        signal aux_pwm_object : out aux_pwm_record
+    ) is
+    begin
+        aux_pwm_object.pwm_is_requested <= true;
+    end start_aux_pwm;
+
+    procedure stop_aux_pwm
+    (
+        signal aux_pwm_object : out aux_pwm_record
+    ) is
+    begin
+        aux_pwm_object.pwm_is_requested <= false;
+    end stop_aux_pwm;
+
+end package body aux_pwm_pkg;
---
+++ source/aux_pwm/tb_aux_pwm.vhd
@@ -0,0 +1,63 @@
+LIBRARY ieee  ; 
+    USE ieee.NUMERIC_STD.all  ; 
+    USE ieee.std_logic_1164.all  ; 
+    use ieee.math_real.all;
+
+    use work.aux_pwm_pkg.all;
+
+library vunit_lib;
+context vunit_lib.vunit_context;
+
+
+entity tb_aux_pwm is
+  generic (runner_cfg : string);
+end;
+
+architecture vunit_simulation of tb_aux_pwm is
+
+    signal simulator_clock : std_logic := '0';
+    constant clock_per : time := 1 ns;
+    constant simtime_in_clocks : integer := 1500;
+
+    signal simulation_counter : natural := 0;
+    -----------------------------------
+    -- simulation specific signals ----
+
+
+    signal aux_pwm : aux_pwm_record := init_aux_pwm;
+    signal pwm_out : std_logic := '0';
+
+begin
+
+------------------------------------------------------------------------
+    simtime : process
+    begin
+        test_runner_setup(runner, runner_cfg);
+        wait for simtime_in_clocks*clock_per;
+        test_runner_cleanup(runner); -- Simulation ends here
+        wait;
+    end process simtime;	
+
+simulator_clock <= not simulator_clock after clock_per/2.0;
+------------------------------------------------------------------------
+
+    stimulus : process(simulator_clock)
+
+    begin
+        if rising_edge(simulator_clock) then
+            simulation_counter <= simulation_counter + 1;
+            create_aux_pwm(aux_pwm);
+
+            if simulation_counter = 50 then
+                start_aux_pwm(aux_pwm);
+            end if; 
+
+            aux_pwm.duty_ratio <= 75;
+            pwm_out <= aux_pwm.pwm_out;
+            aux_pwm.pwm_is_requested <= true;
+
+
+        end if; -- rising_edge
+    end process stimulus;	
+------------------------------------------------------------------------
+end vunit_simulation;
