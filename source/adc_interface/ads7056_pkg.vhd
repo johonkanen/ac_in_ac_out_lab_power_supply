d6d4a1971fa53992a7dda0a944aabd839a86365b
jari <jaarhonka@yahoo.com>
2022-09-18 23:38:50 +0300

    add more functionality to ads7056

--- source/adc_interface/ads7056_pkg.vhd
+++ source/adc_interface/ads7056_pkg.vhd
@@ -15,7 +15,23 @@ package ads_7056_pkg is
     end record;
 
     function init_ads7056 return ads_7056_record;
-
+------------------------------------------------------------------------
+    procedure create_ads7056 (
+        signal ads7056_object : inout ads_7056_record;
+        ad_input_io : std_logic );
+------------------------------------------------------------------------
+    procedure request_ad_conversion (
+        signal ads7056_object : inout ads_7056_record);
+------------------------------------------------------------------------
+    procedure initialize_ads_7056 (
+        signal ads7056_object : inout ads_7056_record);
+------------------------------------------------------------------------
+    function get_ad_measurement ( ads7056_object : ads_7056_record)
+        return integer;
+------------------------------------------------------------------------
+    function ads7056_is_ready ( ads7056_object : ads_7056_record)
+        return boolean;
+------------------------------------------------------------------------
 end package ads_7056_pkg;
 
 
@@ -50,7 +66,8 @@ package body ads_7056_pkg is
 ------------------------------------------------------------------------
     procedure create_ads7056
     (
-        signal ads7056_object : inout ads_7056_record
+        signal ads7056_object : inout ads_7056_record;
+        ad_input_io : std_logic 
     ) is
         alias m is ads7056_object;
     begin
@@ -58,6 +75,11 @@ package body ads_7056_pkg is
         create_clock_divider(m.clock_divider);
 
         if m.clock_divider.clock_counter > 0 then
+
+            if data_delivered_on_rising_edge(m.clock_divider) then
+                m.shift_register <= m.shift_register(m.shift_register'left-1 downto 0) & ad_input_io;
+            end if;
+
             m.chip_select <= '0';
         end if;
     end create_ads7056;
@@ -93,5 +115,30 @@ package body ads_7056_pkg is
         end if;
         
     end initialize_ads_7056;
+------------------------------------------------------------------------
+    function get_ad_measurement
+    (
+        ads7056_object : ads_7056_record
+    )
+    return integer
+    is
+        variable uint_measurement : unsigned(13 downto 0);
+    begin
+
+        uint_measurement := unsigned(ads7056_object.shift_register(15 downto 2));
+
+        return to_integer(uint_measurement);
+        
+    end get_ad_measurement;
+------------------------------------------------------------------------
+    function ads7056_is_ready
+    (
+        ads7056_object : ads_7056_record
+    )
+    return boolean
+    is
+    begin
+        return ads7056_object.is_ready;
+    end ads7056_is_ready;
 ------------------------------------------------------------------------
 end package body ads_7056_pkg;
