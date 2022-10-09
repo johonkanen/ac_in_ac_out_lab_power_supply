5c2656469a591b013bf3c9fd40217de81a3454fb
jari <jaarhonka@yahoo.com>
2022-10-09 21:40:27 +0300

    use subplots for voltages and currents

--- plot_simulation_data.py
+++ plot_simulation_data.py
@@ -7,7 +7,7 @@ import os
 import ltspice
 abs_path = os.path.dirname(os.path.realpath(__file__))
 SimCommander.setLTspiceRunCommand(SimCommander,"c:/Programs/LTC/LTspiceXVII/XVIIx64.exe")
-lcr = SimCommander(abs_path + "./simulation/converter_models/lcr_filter.asc", timeout=20, verbose=False)
+lcr = SimCommander(abs_path + "./simulation/converter_models/lcr_filter.asc", timeout=8, verbose=False)
 lcr.run()
 lcr.wait_completion()
 
@@ -25,8 +25,19 @@ with open('inverter_simulation_results.dat') as f:
         inductor_current.append(float(line_with_separated_contents[1]))
         capacitor_voltage.append(float(line_with_separated_contents[2]))
 
+pyplot.subplot(2, 1, 1)
 pyplot.plot(simulation_time, inductor_current)
-pyplot.plot(simulation_time, capacitor_voltage)
 pyplot.plot(d.get_trace("time"), d.get_trace("I(L1)"))
+pyplot.title('inductor_current')
+pyplot.ylabel('current(A)')
+pyplot.legend(['current from VHDL', 'current from LTSpice'])
+
+pyplot.subplot(2, 1, 2)
+pyplot.plot(simulation_time, capacitor_voltage)
 pyplot.plot(d.get_trace("time"), d.get_trace("V(capacitor_voltage)"))
+pyplot.title('capacitor voltage')
+pyplot.legend(['voltage from VHDL', 'voltage from LTSpice'])
+
+pyplot.xlabel('time (s)')
+pyplot.ylabel('voltage(V)')
 pyplot.show()
