8a56bbdd25c499d7a264769dc2d221ddfc138a52
Jari Honkanen <jaarhonka@yahoo.com>
2025-06-24 15:04:36 +0300

    add titanium top to agilex 3 build

--- quartus_pro/testiAg3top.vhd
+++ quartus_pro/testiAg3top.vhd
@@ -4,8 +4,9 @@ library ieee;
 
 entity testiAg3 is
     port (
-        xclk : in std_logic
-        ;led : out std_logic_vector(2 downto 0)
+        xclk     : in std_logic
+        ;uart_rx : in std_logic
+        ;uart_tx : out std_logic
     );
 end entity testiAg3;
 
@@ -26,6 +27,46 @@ begin
 	port map(refclk => xclk
 				,outclk_0 => core_clock);
 
-    led <= (others => '1');
+    u_titanium_top : entity work.titanium_top
+    port map (
+        main_clock                => core_clock
+        ,pll_locked               => '1'
+        ,uart_rx                  => uart_rx
+        ,uart_tx                  => uart_tx
+        ,grid_inu_leg1_hi         => open
+        ,grid_inu_leg1_low        => open
+        ,grid_inu_leg2_hi         => open
+        ,grid_inu_leg2_low        => open
+        ,dab_primary_hi           => open
+        ,dab_primary_low          => open
+        ,dab_secondary_hi         => open
+        ,dab_secondary_low        => open
+        ,output_inu_leg1_hi       => open
+        ,output_inu_leg1_low      => open
+        ,output_inu_leg2_hi       => open
+        ,output_inu_leg2_low      => open
+        ,primary_bypass_relay     => open
+        ,secondary_bypass_relay   => open
+        ,gate_power1_pwm          => open
+        ,gate_power2_pwm          => open
+        ,gate_power3_pwm          => open
+        ,gate_power4_pwm          => open
+        ,gate_power5_pwm          => open
+        ,gate_power6_pwm          => open
+        ,grid_inu_sdm_clock       => open
+        ,output_inu_sdm_clock     => open
+        ,dab_sdm_clock            => open
+        ,ad_mux1_io               => open
+        ,ads_7056_clock           => open
+        ,ads_7056_chip_select     => open
+        ,ads_7056_input_data      => '1'
+        ,ad_mux2_io               => open
+        ,ads_7056_clock_pri       => open
+        ,ads_7056_chip_select_pri => open
+        ,ads_7056_input_data_pri  => '0'
+        ,grid_inu_sdm_data        => '0'
+        ,output_inu_sdm_data      => '0'
+        ,dab_sdm_data             => '0'
+    );
 
 end rtl;
